4e2a7ff3e108c919f7ba9fbc94ba946ff841b9b3
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-19 15:39:20 +0200

    Revert "yes"
    This reverts commit 605b838041bfe0ceace73cd52475cab141e75cb8.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,10 +3,18 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
-        int LEFT_SPARKMAX_1 = -1;
-        int LEFT_SPARKMAX_2 = -1;
-        int RIGHT_SPARKMAX_1 = -1;
-        int RIGHT_SPARKMAX_2 = -1;
-        int PIGEON_TALON = -1;
+
+    }
+    
+    public interface DIO {
+
+    }
+    
+    public interface PWM {
+
+    }
+    
+    public interface AIN {
+    
     }
 }
